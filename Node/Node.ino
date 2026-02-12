afe0a1232fb8e76d6cb150c1a9421833b30e31e8
YUSUF TOLBERT <ytolber@wgu.edu>
2026-02-12 00:03:35 -0500

    state machine, states added to ino, button transitions markup working

--- Node/Node.ino
+++ Node/Node.ino
@@ -55,10 +55,99 @@ struct EventQue {
     return (count > 0);
   }
 };
+enum class STATEID {
+  setup,
+  monitor,
+  filling,
+  send,
+
+};
+struct StateMachine;
+
+
+struct State {
+  StateMachine *machine;
+  void bind(StateMachine &sm) {
+    machine = &sm;
+  }
+  virtual ~State() = default;
+  virtual void enter() = 0;
+  virtual void run(float) = 0;
+  virtual void exit() = 0;
+};
+
+struct StateSetup : State {
+  void enter() override {
+    Serial.println("Enter Setup state");
+  }
+  void run(float f) override {
+    // Serial.println("Setup state run");
+  }
+  void exit() override {
+    Serial.println("Exit Setup stat");
+  }
+};
+struct StateMonitor : State {
+  void enter() override {
+    Serial.println("Enter Monitor state");
+    // Serial.println(machine->raw);
+  }
+  void run(float f) override {
+    // Serial.println("Monitor state run");
+    
+  }
+  void exit() override {
+    Serial.println("Exit Monitor state");
+  }
+};
+struct StateFill : State {
+  void enter() override {
+    Serial.println("Enter Fill state");
+    // Serial.println(machine->raw);
+  }
+  void run(float f) override {
+    // Serial.println("Fill state run");
+    
+  }
+  void exit() override {
+    Serial.println("Exit Fill state");
+  }
+};
+
+struct StateMachine {
+  float raw;
+  State *current = nullptr;
+  StateSetup setup;
+  StateMonitor monitor;
+  StateFill fill;
+
+  void begin() {
+    setup.bind(*this);
+    monitor.bind(*this);
+    fill.bind(*this);
+    setState(setup);
+  }
+  void setState(State &state) {
+    if (current) current->exit();
+    current = &state;
+    if (current) current->enter();
+    Serial.println(raw);
+  }
+  void run(float r) {
+    raw = r;
+    if (current) current->run(raw);
+  }
+  float getRaw() {
+    return raw;
+  };
+};
+
+
+
 
 struct Transistions {
   EVENT button;
-  char *description;
+  const char *description;
 } transistions[]{
   { EVENT::pressedA, "Button A pressed" },
   { EVENT::pressedB, "Button B pressed" },
@@ -76,10 +165,13 @@ Button btnA;
 Button btnB;
 Button btnC;
 EventQue queue;
+StateMachine machine;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  machine.begin();
+  queue.begin();
   btnA.begin(BUTTON_A);
   btnB.begin(BUTTON_B);
   btnC.begin(BUTTON_C);
@@ -87,19 +179,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (queue.registeredEvent()) {
-    // Serial.println(static_cast<uint8_t>(queue.pop()));
-    transistion_handle(queue.pop());
-  }
-
-  
   if (btnA.pressed()) queue.push(EVENT::pressedA);
   if (btnB.pressed()) queue.push(EVENT::pressedB);
   if (btnC.pressed()) queue.push(EVENT::pressedC);
+
+  if (queue.registeredEvent()) {
+    // Serial.println(static_cast<uint8_t>(queue.pop()));
+    // transistion_handle(queue.pop());
+    EVENT event = queue.pop();
+    transistion_handle(event);
+    switch (event) {
+      case EVENT::pressedA: machine.setState(machine.setup); break;
+      case EVENT::pressedB: machine.setState(machine.monitor); break;
+      case EVENT::pressedC: machine.setState(machine.fill); break;
+    }
+  }
+  static float f = 0.0;
+  machine.run(f);
+  f += 0.01;
 }
 
 void transistion_handle(EVENT event) {
   for (Transistions &t : transistions) {
-    if (t.button == event) Serial.println(t.description);
+    // if (t.button == event) Serial.println(t.description);
   }
 }
